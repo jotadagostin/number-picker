952dfc896275ed0a67e7a81ff22d2c13eae49640
João Paulo Dagostin <dagostinjota@gmail.com>
2025-05-08 21:19:47 +0200

    SETTING
    new design and code

--- javascript/script.js
+++ javascript/script.js
@@ -1,94 +1,95 @@
-//select the draw form:
-const form = document.getElementById("form-draw");
+const numberDraw = document.getElementById("number-draw");
+const numberDrawResults = document.getElementById("number-draw-results");
 
-//select the divisions of interface:
-const resultDiv = document.querySelector(".container-result");
-const formDiv = document.querySelector(".container-form");
+const drawButton = numberDraw.querySelector("button");
+const drawButtonAgain = numberDrawResults.querySelector("button");
+const drawNumbersContainer = document.getElementById("draw-numbers-container");
 
-//fields of user enter:
-const numbersQuantityInput = document.getElementById("numbers-quantity-1"); // how many numbers draw
-const fromInput = document.getElementById("numbers-quantity-2"); // min amount
-const toInput = document.getElementById("numbers-quantity-3"); // max amount
-const switchCheckbox = document.getElementById("switch"); // Checkbox: allowed repetions?
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
 
-//Elements to show the result:
+function buildRandomNumber(min, max, isNotRepeatNumber, uniqueNumbers) {
+  const numberContainer = document.createElement("div");
+  numberContainer.classList.add("number-container");
+  const span = document.createElement("span");
 
-const numbersContainer = document.querySelector(".container-result-numbers");
-const resultSubtitle = document.getElementById("result-subtitle");
-const drawAgainButton = document.getElementById("draw-again");
+  let randomNumber;
+  if (isNotRepeatNumber) {
+    let isNotNewNumber;
+    do {
+      randomNumber = getRandomIntInclusive(min, max);
+      isNotNewNumber = uniqueNumbers.includes(randomNumber);
+    } while (isNotNewNumber);
 
-//when click in "draw again" reset the page:
+    uniqueNumbers.push(randomNumber);
+  } else {
+    randomNumber = getRandomIntInclusive(min, max);
+  }
 
-drawAgainButton.addEventListener("click", resetPage);
+  span.innerText = String(randomNumber);
+  numberContainer.appendChild(span);
+  drawNumbersContainer.appendChild(numberContainer);
+}
 
-//When the form is sent, execut the draw:
-form.addEventListener("submit", function (event) {
-  event.preventDefault(); // //block the standart behavio(reload the page)
-  drawNumbers(); //call the function draw number
-});
-
-//main function: draw the numbers:
+function changeQuantityOfDraws() {
+  const resultNumber = document.getElementById("result-number");
+  resultNumber.innerText = Number(resultNumber.innerText) + 1;
+}
 
 function drawNumbers() {
-  const quantity = parseInt(numbersQuantityInput.value); // Convert the enter number
-  const from = parseInt(fromInput.value);
-  const to = parseInt(toInput.value);
+  changeQuantityOfDraws();
 
-  // Validate: block the invalid amounts
-  if (quantity < 1 || from >= to) {
-    alert("Please, insert a valid number.");
-    return;
+  let numbersQuantity = document.getElementById("numbers-quantity").value;
+  const minNumber = Number(document.getElementById("min-number").value);
+  const maxNumber = Number(document.getElementById("max-number").value);
+
+  const uniqueNumbers = [];
+  const isNotRepeatNumber = document.getElementById("switch").checked;
+
+  if (isNotRepeatNumber && numbersQuantity > maxNumber) {
+    numbersQuantity = maxNumber;
   }
 
-  let numbers = []; // Final list random numbers
-  let uniqueNumbers = new Set(); // to set that there is no repeat
+  buildRandomNumber(minNumber, maxNumber, isNotRepeatNumber, uniqueNumbers);
 
-  // Loop to draw the numbers
-  while (numbers.length < quantity) {
-    let randomNumber = Math.floor(Math.random() * (to - from + 1)) + from;
+  const delayInMilliseconds = 2500; // 2.5 seconds
 
-    if (switchCheckbox.checked) {
-      // if repeat is not allowed, add is still does not exist
-      if (!uniqueNumbers.has(randomNumber)) {
-        uniqueNumbers.add(randomNumber);
-        numbers.push(randomNumber);
-      }
-    } else {
-      // if it can repeat, just add directly
-      numbers.push(randomNumber);
-    }
+  for (let index = 2; index <= numbersQuantity; index++) {
+    setTimeout(
+      () =>
+        buildRandomNumber(
+          minNumber,
+          maxNumber,
+          isNotRepeatNumber,
+          uniqueNumbers
+        ),
+      delayInMilliseconds * (index - 1)
+    );
   }
 
-  // Show the result in the scream
-  displayResults(numbers);
+  setTimeout(
+    () => (drawButtonAgain.style.display = "flex"),
+    numbersQuantity * delayInMilliseconds
+  );
 }
 
-//function to show the results:
-function displayResults(numbers) {
-  formDiv.style.display = "none"; //hide the form
-  resultDiv.style.display = "flex"; //show the result
+drawButton.onclick = (event) => {
+  event.preventDefault();
 
-  //add the numbers to the HTML:
-  numbersContainer.innerHTML = numbers
-    .map((num) => `<span class="number">${num}</span>`)
-    .join("");
+  numberDraw.style.display = "none";
+  numberDrawResults.style.display = "flex";
 
-  //show the subtitle of the quantity drawed:
-  resultSubtitle.textContent = `${numbers.length} Numbers Draw`;
+  drawNumbers();
+};
 
-  //after 1 second, add the class to animate the numbers
-  setTimeout(() => {
-    document
-      .querySelectorAll(".container-result-numbers span")
-      .forEach((span) => span.classList.add("show-number"));
-  }, 1000);
-}
+drawButtonAgain.onclick = (event) => {
+  event.preventDefault();
 
-//function to reset the page:
-function resetPage() {
-  resultDiv.style.display = "none"; //hide the results
-  formDiv.style.display = "flex"; //show the form again
-  numbersContainer.innerHTML = ""; //clear the last numbers
-  resultSubtitle.textContent = ""; //clear the subtitle
-  form.reset(); //reset all the forms
-}
+  drawButtonAgain.style.display = "none";
+  drawButtonAgain.style.opacity = 0;
+  drawNumbersContainer.innerHTML = "";
+  drawNumbers();
+};
